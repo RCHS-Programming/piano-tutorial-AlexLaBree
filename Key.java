914244df0cb6ca33d98e9b473e7b3675b7ff3a3e
Labree <75798879+AlexLaBree@users.noreply.github.com>
2021-04-19 12:27:18 -0400

    first G key plays note

--- Key.java
+++ Key.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
     /**
      * Create a new key.
      */
@@ -14,5 +15,25 @@ public class Key extends Actor
      */
     public void act()
     {
+
+        if(!isDown&&Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key-down.png");
+            isDown=true;
+        }
+        if(isDown&&!Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key.png");
+            isDown=false;
+        }
     }
-}
+    
+    /*
+     * Play the note of this key
+     */
+    public void play()
+    {
+        Greenfoot.playSound("3a.wav");
+    }
+}   
+     
