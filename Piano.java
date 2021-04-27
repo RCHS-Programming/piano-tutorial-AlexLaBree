bb67b25b7cfe4fdef6d7d70bb2832916c20b8b3a
Labree <75798879+AlexLaBree@users.noreply.github.com>
2021-04-27 12:28:21 -0400

    added unique key and sounds

--- Piano.java
+++ Piano.java
@@ -8,13 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[] whitekeys = 
+       {"a","s","d","f","g","h","j","k","i",";","'","\\"};
+    private String[] whiteNotes = 
+       {"3c","3d","3e","3f","3g","3a","3b","4c","4d","4e","4f","4g"};
     /**
      * Make the piano.
      */
     public Piano() 
-    {
+   {
         super(800, 340, 1);
         
-        addObject (new Key ("g", "3a.wav"), 300, 180);
-    }
-}
+        int i;
+        
+        for (i=0;i<whitekeys.length; i++)
+        {
+            Key key=new Key(whiteKeys[i], whiteNotes[i]+".wav");
+            addObject(key,54 + i*63, 140);
+      }
+   }
+} 
